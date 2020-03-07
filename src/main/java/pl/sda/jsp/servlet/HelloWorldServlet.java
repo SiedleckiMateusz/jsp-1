a59d489a4864bfc27c6f08eda59e8613fdf60a97
Mateusz <siedleckimateus@gmail.com>
2020-03-07 10:41:25 +0100

    zadanie 2 - few urlPattern and initParam

--- src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/jsp/servlet/HelloWorldServlet.java
@@ -1,5 +1,6 @@
 package pl.sda.jsp.servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/","/hello"})
 public class HelloWorldServlet extends HttpServlet {
+
+    private String id, localization;
+
+    @Override
+    public void init(ServletConfig config){
+        id = config.getInitParameter("id");
+        localization = config.getInitParameter("localization");
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,6 +29,8 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         final PrintWriter writer = response.getWriter();
 
-        writer.println("<h1>Hello World</h1>");
+        writer.println("<h1>Hello World</h1><br>");
+        writer.println("Id: "+id+"<br>");
+        writer.println("Lokalizacja: "+localization);
     }
 }
